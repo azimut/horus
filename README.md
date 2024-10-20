386ed5e05c0af7668b850bfd3629cf411e260940
azimut <azimut.github@protonmail.com>
2024-10-20 02:41:20 -0300

    safe-exceptions and screenshoot MVP

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 ## References
-- [Haskell code to screenshoot a ppm](https://github.com/xmonad/X11/blob/master/examples/ScreenCapture.hs)
 - [C code example using x11+cairo](https://chromium.googlesource.com/chromiumos/platform/window_manager/+/a2bc12e3761d2c1d41b69c2a334af60262e7bd4c/screenshot.cc)
+- [Haskell code to screenshoot a ppm](https://github.com/xmonad/X11/blob/master/examples/ScreenCapture.hs)
+- [Haskell code sdl2+cairo](https://github.com/haskell-gi/haskell-gi/blob/master/cairo/examples/sdl/CairoSDL.hs)
+- [Haskell casting of Image](https://github.com/ndzik/horture/blob/2e631613e97ef1fb305de061c4e04ca3c8fced4c/src/Horture/Backend/X11/X11.hs#L37)
--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,47 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TypeApplications #-}
 
 module Main where
 
-import Control.Exception (bracket)
+import Control.Exception.Safe (bracket)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Managed (managed, runManaged)
+import qualified Foreign as F
 import Foreign.C (CInt, CUInt (CUInt), CULong (CULong))
+import qualified Foreign.C as F
 import qualified Graphics.Rendering.Cairo as C
 import Graphics.X11.Xlib
 import Graphics.X11.Xlib.Extras (WindowAttributes (wa_height, wa_width), getWindowAttributes)
+import Graphics.X11.Xlib.Types (Image (..))
 
 main :: IO ()
 main = runManaged $ do
   display <- managed withDisplay
   (w, h) <- liftIO $ getRootDimensions display
   image <- managed (withImage display (cuint w) (cuint h))
-  surface <- managed (C.withImageSurface C.FormatRGB24 (int w) (int h))
+  -- surface <- managed (C.withImageSurface C.FormatRGB24 (int w) (int h))
+  -- type PixelData = Ptr CUChar
+  -- newtype Image = Image (Ptr Image)
+  real <- liftIO $ wut image
+  strides <- liftIO $ fromIntegral <$> www image
+  surface <- managed (C.withImageSurfaceForData real C.FormatARGB32 (int w) (int h) strides)
   liftIO $ do
     C.surfaceWriteToPNG surface "/home/sendai/test.png"
-    putStrLn "foo"
+    putStrLn "Done!"
   where
     cuint i = fromIntegral i :: CUInt
     int i = fromIntegral i :: Int
+    www (Image p) = F.peekByteOff @F.CInt (F.castPtr p) (4 * szCInt + szPtr + 5 * szCInt)
+    wut (Image p) = F.castPtr <$> F.peek (F.plusPtr @Image @(F.Ptr F.CIntPtr) p (4 * szCInt))
+
+szCInt :: Int
+szCInt = F.sizeOf @F.CInt undefined
+
+szPtr :: Int
+szPtr = F.sizeOf @F.CIntPtr undefined
+
+szCULong :: Int
+szCULong = F.sizeOf @F.CULong undefined
 
 withImage :: Display -> CUInt -> CUInt -> (Image -> IO r) -> IO r
 withImage display w h f = do
