b27293efdb5c58d9e976af50187b28e22f70a522
azimut <azimut.github@protonmail.com>
2024-10-23 16:07:16 -0300

    wipe bmp save prototype from code

--- src/Store.hs
+++ src/Store.hs
@@ -1,20 +1,12 @@
 {-# LANGUAGE RecordWildCards #-}
 
-module Store (savePng, saveBmp) where
+module Store (savePng) where
 
-import Codec.BMP as BMP
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Managed (managed, runManaged)
-import qualified Data.ByteString.Unsafe as BS
 import qualified Foreign as F
 import qualified Graphics.Rendering.Cairo as C
 import Win
-
-saveBmp :: RawImage a -> FilePath -> IO ()
-saveBmp RawImage {..} filename = do
-  bytes <- BS.unsafePackMallocCStringLen (F.castPtr rawImagePtr, rawImageSizeInBytes)
-  BMP.writeBMP filename $
-    BMP.packRGBA32ToBMP32 rawImageWidth rawImageHeight bytes
 
 savePng :: RawImage a -> FilePath -> IO ()
 savePng RawImage {..} filename = runManaged $ do
